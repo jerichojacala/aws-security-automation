8236af448c97a4c7ff1415e3398e44b47204a3fd
jerichojacala <jjacala@bu.edu>
2026-09-11 19:40:39 -0400

    added dummy public test file to upload to bucket, updated terraform files, updated readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # aws-security-automation
+
+## Problems I ran into (to talk about in interviews)
+
+1. Race conditions
+
+Before setting out on this project, I had never used terraform to build infrastructure where components relied on each other. During this project, I ran into an issue where I tried to create a public policy while simultaneously disabling the aws public access block. When I tried to deploy the system, I ran into an error, read it, developed a theory that this was the case. Based off of my limited knowledge of Terraform I asked myself, *At a high level, how would Terraform execute the code I gave it? How might that cause problems?* I recognized the potential of a race condition, searched for other potential issues, fed the error message to an LLM to corroborate, and cross-checked the recommended fix with Terraform documentation. From there, the issue was quickly solved.
--- main.tf
+++ main.tf
@@ -1,16 +1,50 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 6.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "jjacala-test-bucket"
+resource "aws_s3_bucket" "security_demo" {
+  bucket_prefix = "jjacala-security-demo-"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "security_demo" {
+  bucket = aws_s3_bucket.security_demo.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "security_demo" {
+  bucket = aws_s3_bucket.security_demo.id
+  depends_on = [aws_s3_bucket_public_access_block.security_demo]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid    = "PublicRead"
+        Effect = "Allow"
+
+        Principal = "*"
+
+        Action = [
+          "s3:GetObject"
+        ]
+
+        Resource = "${aws_s3_bucket.security_demo.arn}/*"
+      }
+    ]
+  })
 }
